f9c77f024d1ecd76ebdb1ed5ff0249b53d7ed7f3
pravar <pravar.sharma@trootech.com>
2023-03-20 14:40:39 +0530

    fetch the genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,28 +1,28 @@
 import { useEffect, useState } from "react";
 import apiClient, { CanceledError } from "../services/api-client";
-import { FetchGamesResponse, Games } from "../services/game";
+import { FetchGenreResponse, Genre } from "../services/genre";
 
-const useGames = () => {
-  const [games, setGames] = useState<Games[]>([]);
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    const controller = new AbortController();
+    let controller = new AbortController();
     setIsLoading(true);
     apiClient
-      .get<FetchGamesResponse>("games", { signal: controller.signal })
+      .get<FetchGenreResponse>("genres", { signal: controller.signal })
       .then((res) => {
-        setGames(res.data.results);
+        setGenres(res.data.results);
         setIsLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setIsLoading(false);
         setError(err.message);
+        setIsLoading(false);
       });
     return () => controller.abort();
   }, []);
-  return { games, error, isLoading };
+  return { genres, error, isLoading, setGenres };
 };
 
-export default useGames;
+export default useGenres;
---
+++ src/services/genre.ts
@@ -0,0 +1,12 @@
+import create from "./http-services";
+
+export interface Genre{
+    id: number,
+    name: string,
+    slug: string
+}
+export interface FetchGenreResponse{
+    results: Genre[]
+}
+
+export default create('genres')
