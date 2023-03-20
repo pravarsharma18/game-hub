f85ce5d898339f3b68f2b76cf0a87f5a17dcbfe7
pravar <pravar.sharma@trootech.com>
2023-03-20 15:22:08 +0530

    show sipnner while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data, error } = useGenres();
+  const { data, error, isLoading } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <>
-      {error && <Text>{error}</Text>}
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
