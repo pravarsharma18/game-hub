6417cab9298745589e912113b5469c0d62a643b7
pravar <pravar.sharma@trootech.com>
2023-03-20 14:11:03 +0530

    refactor remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Games } from "../services/game";
 import getCroppedUrl from "../services/image-url";
 import CriticBadge from "./CriticBadge";
+import GameCardContainer from "./GameCardContainer";
 import PlatformIconList from "./PlatformIconList";
 
 interface Game {
@@ -9,18 +10,20 @@ interface Game {
 }
 const GameCard = ({ game }: Game) => {
   return (
-    <Card borderRadius={10} overflow="hidden" width="300px">
-      <Image src={getCroppedUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticBadge score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image src={getCroppedUrl(game.background_image)} />
+        <CardBody>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <HStack justifyContent="space-between">
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticBadge score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
