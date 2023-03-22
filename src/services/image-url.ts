beeccda51fe4f07897ce30fc6195590ebc93ee5c
pravar <pravar.sharma@trootech.com>
2023-03-22 14:09:47 +0530

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/No_Image_Available.jpg";
 const getCroppedUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   let target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
